730fa2747e2bd5c488e9a7402b11066bef879236
hagerahmed2711 <hager.ahmed.200747@gmail.com>
2023-08-10 15:50:10 -0700

    done

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/***/
+
+int main(void)
+{
+	printf("Size of a char", sizeof(char));
+	printf("Size of an int", sizeof(int));
+	printf("Size of a long", sizeof(long));
+	printf("Size of a long long int", sizeof(long long int));
+	printf("Size of a float", sizeof(float));
+	return (0);
+}
