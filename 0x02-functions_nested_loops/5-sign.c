1bbd2267a4a631de9b6b9a95c9b7e91d3eb8660b
hagerahmed2711 <hager.ahmed.200747@gmail.com>
2023-08-16 16:48:49 -0700

    done

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,13 +12,19 @@
 int print_sign(int n)
 {
 	if (n > 0)
-		_putchar('+');
+	{	
+		_putchar(43);
 		return (1);
-	if (n == 0)
+	}
+	else if (n == 0)
+	{
 		_putchar('0');
 		return (0);
-	if (n < 0)
+	}
+	else (n < 0)
+	{
 		_putchar('-');
 		return (-1);
+	}
 
 }
