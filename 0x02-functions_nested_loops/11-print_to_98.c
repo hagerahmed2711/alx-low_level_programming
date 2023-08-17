d8efd3b9f1711fabef44e13a04833cf4dc3fba57
hagerahmed2711 <hager.ahmed.200747@gmail.com>
2023-08-16 17:30:46 -0700

    done

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,14 +15,14 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d", i);
+			printf("%d, ", i);
 		}
 	}
 	else
 	{
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d", i);
+			printf("%d, ", i);
 		}
 	}
 	printf("\n");
