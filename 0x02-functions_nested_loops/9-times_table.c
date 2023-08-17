6a0810f8778fc575f3b0883d98198e79a07b49f1
hagerahmed2711 <hager.ahmed.200747@gmail.com>
2023-08-16 17:03:18 -0700

    done

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,8 +18,8 @@ void times_table(void)
 			if (product < 9)
 				_putchar(' ');
 			else
-				_putchar('(product / 10)');
-			_putchar('(product % 10)');
+				_putchar(product / 10 + '0');
+			_putchar(product % 10 + '0');
 			_putchar(',');
 			_putchar(' ')
 		}
