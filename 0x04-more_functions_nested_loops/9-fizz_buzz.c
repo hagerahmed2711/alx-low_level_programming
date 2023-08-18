a6f554d22764d15b67c09ac6b39e34edd5a62737
hagerahmed2711 <hager.ahmed.200747@gmail.com>
2023-08-17 17:19:21 -0700

    done

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ *
+ * Return:always 0 (success).
+ *
+*/
+
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 100; num++)
+	{
+		if (num % 3 == 0 && !(num % 5 == 0))
+			printf("Fizz");
+		else if (num % 5 == 0 && !(num % 3 == 0))
+			printf("Buzz");
+		else if (num % 5 == 0 && num % 3 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", num);
+		if (num != 100)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
+}
+
