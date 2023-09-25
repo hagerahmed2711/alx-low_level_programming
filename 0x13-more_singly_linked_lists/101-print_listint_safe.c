132c82572eab7b314ffd23d7f6378a58d2a4344f
hagerahmed2711 <hager.ahmed.200747@gmail.com>
2023-09-25 10:28:06 -0700

    done

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,30 +1,69 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints a linked list.
- * @head: the head of the linked list
+ * free_linked_list - frees a linked list
+ * @head: head of the linked list.
  *
- * Return: number of nodes in the list
+ * Return: void.
  */
+void free_linked_list(listp_t **head)
+{
+	listp_t *temp_node;
+	listp_t *current_node;
 
+	if (head != NULL)
+	{
+		current_node = *head;
+		while ((temp_node = current_node) != NULL)
+		{
+			current_node = current_node->next;
+			free(temp_node);
+		}
+		*head = NULL;
+	}
+}
+
+/**
+ * print_listint_safe - prints a linked list.
+ * @head: head of the linked list.
+ *
+ * Return: number of nodes in the list.
+ */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t number = 0;
-	long int difference;
+	size_t no_nodes = 0;
+	listp_t *h, *new_node, *add_node;
 
-	while (head)
+	h = NULL;
+	while (head != NULL)
 	{
-		difference = head - head->next;
-		number++;
-		printf("[%p] %d\n", (void *)head, head->n);
-		if (difference > 0)
-			head = head->next;
-		else
+		new_node = malloc(sizeof(listp_t));
+
+		if (new_node == NULL)
+			exit(98);
+
+		new_node->p = (void *)head;
+		new_node->next = h;
+		h = new_node;
+
+		add_node = h;
+
+		while (add_node->next != NULL)
 		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			break;
+			add_node = add_node->next;
+			if (head == add_node->p)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_linked_list(&h);
+				return (no_nodes);
+			}
 		}
+
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		no_nodes = no_nodes + 1;
 	}
 
-	return (number);
+	free_linked_list(&h);
+	return (no_nodes);
 }
